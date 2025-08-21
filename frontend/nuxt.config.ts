8fab29d689633cc81d9442f55272d9fca0a2ccc4
KrTvGG <vanyusha.krutov2@bk.ru>
2025-08-21 20:48:54 +0300

    настройка фронта

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,6 +1,68 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/icon', '@nuxt/image', '@nuxt/eslint']
+  modules: [
+    '@nuxt/icon', 
+    '@nuxt/image', 
+    '@nuxt/eslint'
+  ],
+  app: {
+    head: {
+      charset: 'utf-8',
+      htmlAttrs: {
+        lang: 'ru',
+      },
+    }
+  },
+  css: [
+    "~/assets/scss/reset.scss",
+    "~/assets/scss/additional.scss",
+  ],
+  vite: {
+    resolve: {
+      alias: {
+        '@assets': path.resolve(__dirname, 'assets'),
+      }
+    },
+    plugins: [
+      tsconfigPaths(),
+      {
+        name: 'vite-plugin-glob-transform',
+        transform(code: string, id: string) {
+          if (id.includes('nuxt-icons')) {
+            return code.replace(/as:\s*['"]raw['"]/g, 'query: "?raw", import: "default"')
+          }
+          return code
+        }
+      }
+    ],
+    css: {
+      preprocessorOptions: {
+        scss: {
+          api: 'modern-compiler',
+          silenceDeprecations: ["legacy-js-api"],
+          additionalData: `
+              @use "~/assets/scss/runtime/modules.scss" as *; 
+              @use '~/assets/scss/runtime/mixins' as *;
+          `,
+          
+        }
+      }
+    },
+    server: {
+      watch: {
+        usePolling: true,
+        interval: 500,
+      },
+    }
+  },
+  watchers: {
+    webpack: {
+      aggregateTimeout: 500,
+    }
+  }
 })
