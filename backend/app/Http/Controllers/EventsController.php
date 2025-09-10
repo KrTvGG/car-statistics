bc9fa03078a0174867838798816b71195d68959a
ps_krutov_i <krutov.ivan@place-start.ru>
2025-09-10 20:56:14 +0300

    создан CRUD API для собылий

---
+++ backend/app/Http/Controllers/EventsController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\EventsRequest;
+use App\Http\Resources\EventsResource;
+use App\Models\Events;
+
+class EventsController
+{
+    public function __construct() {}
+
+    /**
+     * Display a listing of the resource.
+     * GET /api/v1/events
+     */
+    public function index()
+    {
+        $query = Events::filter(request()->all());
+        return EventsResource::collection($query->get());
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * POST /api/v1/events
+     */
+    public function store(EventsRequest $request)
+    {
+        $event = Events::create($request->validated());
+        return new EventsResource($event);
+    }
+
+    /**
+     * Display the specified resource.
+     * GET /api/v1/events/{id}
+     */
+    public function show(Events $event)
+    {
+        return new EventsResource($event);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     * PUT /api/v1/events/{id}
+     */
+    public function update(EventsRequest $request, Events $event)
+    {
+        $event->update($request->validated());
+        return new EventsResource($event);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     * DELETE /api/v1/events/{id}
+     */
+    public function destroy(Events $event)
+    {
+        $event->delete();
+        return response()->json(null, 204);
+    }
+}
--- backend/app/Models/Events.php
+++ backend/app/Models/Events.php
@@ -13,4 +13,13 @@ class Events extends Model
     protected $fillable = [
         'name'
     ];
+
+    protected $labels = [
+        'name' => 'Наименование события',
+    ];
+
+    public function scopeFilter($query, array $filters)
+    {
+        return $query->when($filters['name'] ?? null, fn($q, $name) => $q->where('name', 'like', "%$name%"));
+    }
 }
